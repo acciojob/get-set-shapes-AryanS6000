001370e304e69eaa58922931a5120577664cd104
AryanS6000 <163299006+AryanS6000@users.noreply.github.com>
2025-07-23 12:57:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-//complete this code
 class Rectangle {
-	constructor(width,height){
-		this.width = _width;
-		this.height = _height;
-	}
-	 get width(){
-		 return this._width;
-	 }
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-	get height(){
-		 return this._height;
-	 }
-	get area(){
-		return this._width * this._height;
-	}
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  get area() {
+    return this._width * this._height;
+  }
 }
 
-	class Square extends Rectangle {
-		constructor(side){
-		super(side,side)
-		}
-		get perimeter(){
-			return 4* this._width;
-		}
-	}
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
+
+  get perimeter() {
+    return 4 * this._width;
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+
+ 
